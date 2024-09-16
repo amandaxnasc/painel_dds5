dbb3306ffd2e3bdf48c685ea864983f8df539add
AMANDA XAVIER NASCIMENTO <a.nascimento@labcetec.net>
2024-09-16 17:07:42 -0300

    Criando Tabelas

---
+++ client/src/components/tabelaAulas/AbreviaInstrutor.jsx
@@ -0,0 +1,8 @@
+function AbreviaInstrutor (props) {
+    let nome = new String(props.instrutor).split(' ');
+    nome = nome[0] + ' '+ nome[nome.length -1];
+    return nome;
+}
+
+
+export default AbreviaInstrutor;
